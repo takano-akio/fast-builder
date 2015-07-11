668fc2df0d1d9a227f3eae37669df52a19e8f911
Takano Akio <aljee@hyper.cx>
2015-07-12 01:37:13 +0900

    benchmarks/aeson: runInUnboundThread

--- benchmarks/aeson/main.hs
+++ benchmarks/aeson/main.hs
@@ -1,12 +1,13 @@
 import qualified Fast as F
 import qualified Bstr as B
+import Control.Concurrent
 import Control.DeepSeq
 import Criterion.Main
 import Data.Aeson (decode', encode)
 import qualified Data.ByteString.Lazy as L
 
 main :: IO ()
-main = do
+main = runInUnboundThread $ do
   Just json <- decode' <$> L.readFile "../aeson/benchmarks/json-data/twitter100.json"
   rnf json `seq` return ()
   defaultMain
